a97b19c2abc1ff4dbf16d8ff1feb2d21175a0868
mikhail <michael.maidan1998@gmail.com>
2024-08-15 20:04:53 +0200

    build configuration was done

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,41 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import autoprefixer from 'autoprefixer';
+
+
+const resolveAlias = (p) => resolve(__dirname, p);
+const alias = { 
+  "@": resolveAlias('./src'),
+  "~": resolveAlias('./'),
+}
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    viteStaticCopy({
+      targets: [
+        {
+          src: './src/assets',
+          dest: './'
+        }
+      ]
+    }),
+  ], 
   server: {
     open: true,
   },
+  css: {
+    postcss: {
+      plugins: [ 
+        autoprefixer({}),
+      ]
+    }
+  },
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias,
   },
 });
 
